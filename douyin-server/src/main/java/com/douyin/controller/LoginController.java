5b8788eaa6fd7c06702873b9381f758a38953ab3
liubowen <1011434379@qq.com>
2025-02-13 18:14:01 +0800

    抖音电商项目2.13用户服务刘博文

--- douyin-server/src/main/java/com/douyin/controller/LoginController.java
+++ douyin-server/src/main/java/com/douyin/controller/LoginController.java
@@ -40,22 +40,27 @@ public class LoginController {
         user.setUsername(username);
         user.setPassword(loguser.getPassword());
 
+        //查询用户名和密码是否正确
         User u = userService.login(user);
 
-        //登录成功  生成令牌 下发令牌
+        //用户名和密码正确
         if(u != null){
-            Map<String, Object> claims = new HashMap<>();
-            claims.put("id", u.getId());
-            claims.put("name", u.getName());
-            claims.put("username", u.getUsername());
+            if(redisTemplate.opsForValue().get(username) == null){//用户之前未登录
+                Map<String, Object> claims = new HashMap<>();
+                claims.put("id", u.getId());
+                claims.put("name", u.getName());
+                claims.put("username", u.getUsername());
 
-            String jwt = JwtUtils.generateJwt(claims); //jwt包含了当前登录用户的信息
+                String jwt = JwtUtils.generateJwt(claims); //jwt包含了当前登录用户的信息
 
-            //将jwt令牌和对应的用户名存入redis中，key为username，value为对应的jwt，并设置相同的有效期
-            redisTemplate.opsForValue().set(u.getUsername(), jwt, 12, TimeUnit.HOURS);
-            String o = (String) redisTemplate.opsForValue().get(u.getUsername());
-            System.out.println(o);
-            return Result.success(jwt);
+                //将jwt令牌和对应的用户名存入redis中，key为username，value为对应的jwt，并设置相同的有效期
+                redisTemplate.opsForValue().set(u.getUsername(), jwt, 12, TimeUnit.HOURS);
+                String o = (String) redisTemplate.opsForValue().get(u.getUsername());
+                System.out.println(o);
+                return Result.success(jwt);
+            }else{//用户之前已登录
+                return Result.success("用户已登录");
+            }
         }
 
         return Result.success("用户名或密码错误");
