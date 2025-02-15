7d517086af6e9f1f63e19dda817f55c55eb0a960
Administrator <yixuwang01@gmail.com>
2025-02-15 10:12:22 +0800

    test0215

---
+++ douyin-pojo/src/main/java/com/douyin/dto/ShoppingCartDTO.java
@@ -0,0 +1,17 @@
+package com.douyin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShoppingCartDTO implements Serializable {
+    private Long product_id;
+
+}
