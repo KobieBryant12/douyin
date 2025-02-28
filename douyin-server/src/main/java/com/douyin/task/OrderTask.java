b880e5ac1d8e848602a436cce7c1db529ff45890
liubowen <1011434379@qq.com>
2025-02-28 09:22:42 +0800

    抖音电商项目2.28刘博文

--- douyin-server/src/main/java/com/douyin/task/OrderTask.java
+++ douyin-server/src/main/java/com/douyin/task/OrderTask.java
@@ -22,19 +22,19 @@ public class OrderTask {
     /**
      * 超时订单处理
      */
-//    @Scheduled(cron = "0/5 * * * * ? ")
-//    public void outOfTime() {
-////        log.info("订单超时，定时处理", LocalDateTime.now());
-//        List<OrderAndDetail> ordersList = orderMapper.getByStatusOrderTimeLT((short)0, LocalDateTime.now().plusMinutes(-1));
-//        if(ordersList != null && ordersList.size() > 0){
-//            for (OrderAndDetail orders : ordersList) {
-//                orders.setPayStatus((short)2);
-//                orders.setCancelTime(LocalDateTime.now());
-//                orderMapper.update(orders);//更新订单状态
-//            }
-//        }
-//
-//    }
+    @Scheduled(cron = "0/5 * * * * ? ")
+    public void outOfTime() {
+//        log.info("订单超时，定时处理", LocalDateTime.now());
+        List<OrderAndDetail> ordersList = orderMapper.getByStatusOrderTimeLT((short)0, LocalDateTime.now().plusMinutes(-1));
+        if(ordersList != null && ordersList.size() > 0){
+            for (OrderAndDetail orders : ordersList) {
+                orders.setPayStatus((short)2);
+                orders.setCancelTime(LocalDateTime.now());
+                orderMapper.update(orders);//更新订单状态
+            }
+        }
+
+    }
 
 //    /**
 //     * 定时处理一直派送的订单
